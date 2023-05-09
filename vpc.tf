1b0e57636988af61a0080a365e83638a3fbc9c5d
gpalaciosg-at-243505175593 <guipalaga@gmail.com>
2023-05-09 12:41:12 -0600

    fix: Removinf private subnets from vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -7,7 +7,6 @@ module "vpc" {
   azs                          = data.aws_availability_zones.this.names
   create_database_subnet_group = var.create_database_subnet_group
   public_subnets               = var.public_subnet_cidrs
-  private_subnets              = var.private_subnet_cidrs
   database_subnets             = var.database_subnets_cidrs
 
   tags = var.tags
