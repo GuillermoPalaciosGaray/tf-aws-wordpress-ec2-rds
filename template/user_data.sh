00d8d9ca6e74a24c52314dcd8596b221ed620c4d
gpalaciosg-at-243505175593 <guipalaga@gmail.com>
2023-05-09 19:48:17 -0600

    fix: Updating  user_data.sh

--- template/user_data.sh
+++ template/user_data.sh
@@ -3,9 +3,10 @@ sudo su
 yum -y update
 yum install -y httpd
 yum install -y docker
+yum install -y mariadb-server
 systemctl enable docker
 service docker start
 usermod -a -G docker ec2-user
 systemctl enable docker.service
 sudo docker pull wordpress
-sudo docker run -d -p 80:80 -p 443:443 wordpress
+sudo docker run -d -p 80:80 -p wordpress 
